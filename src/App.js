b060db3c8e4acd9e2c386486e8ec62a9e9e68d56
Braden Chapman <bradenchapman@Bradens-MacBook-Air.local>
2024-03-26 12:12:39 -0600

    wire up BookCreate component with state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import BookCreate from "./BookCreate";
+
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState({});
+
+  const createBook = (title) => {
+    console.log("add book with title", title);
+  };
+
+  return (
+    <div>
+      App
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
