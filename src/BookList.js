5e135f90cac70025a1bf58a4f944efda6d16e460
Braden Chapman <bradenchapman@Bradens-MacBook-Air.local>
2024-03-27 14:47:37 -0600

    refactor app to get useContext working

--- src/BookList.js
+++ src/BookList.js
@@ -1,12 +1,21 @@
+import { useContext } from "react";
 import BookShow from "./BookShow";
+import BooksContext from "./Context/books";
 
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
     );
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>up</button>
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
